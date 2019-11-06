af9e9f770bb772ebe94ad92d8729d2559ca8891d
Zsolt Gál <zsolt.gal@sessiondigital.com>
2019-11-06 13:25:55 +0100

    Remove lock, require global composer autoloader

--- src/Extension.php
+++ src/Extension.php
@@ -1,51 +1,55 @@
 <?php
 
-namespace Technodelight\JiraTempoExtension;
+namespace {
+    require_once getenv('HOME') . '/.composer/vendor/autoload.php';
+}
 
-use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
-use Symfony\Component\Config\Definition\Builder\TreeBuilder;
-use Symfony\Component\Config\FileLocator;
-use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
-use Technodelight\Jira\Extension\ExtensionInterface;
-
-class Extension implements ExtensionInterface
+namespace Technodelight\JiraTempoExtension
 {
-    public function load(array $configs, ContainerBuilder $container)
+    use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
+    use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+    use Symfony\Component\Config\FileLocator;
+    use Symfony\Component\DependencyInjection\ContainerBuilder;
+    use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+    use Technodelight\Jira\Extension\ExtensionInterface;
+
+    class Extension implements ExtensionInterface
     {
-        $loader = new XmlFileLoader($container, new FileLocator(__DIR__ . '/Resources'));
-        $loader->load('services.xml');
+        public function load(array $configs, ContainerBuilder $container)
+        {
+            $loader = new XmlFileLoader($container, new FileLocator(__DIR__ . '/Resources'));
+            $loader->load('services.xml');
 
-        $def = $container->getDefinition('technodelight.jira.config.integrations.tempo');
-        $def->setArguments(
-            [isset($configs['tempo']) ? $configs['tempo'] : ['instances' => []]]
-        );
-    }
+            $def = $container->getDefinition('technodelight.jira.config.integrations.tempo');
+            $def->setArguments(
+                [isset($configs['tempo']) ? $configs['tempo'] : ['instances' => []]]
+            );
+        }
 
-    public function configure(): ArrayNodeDefinition
-    {
-        $builder = new TreeBuilder;
-        $node = $builder->root('tempo');
+        public function configure(): ArrayNodeDefinition
+        {
+            $builder = new TreeBuilder;
+            $node = $builder->root('tempo');
 
-        $node
-            ->info('Tempo timesheets (https://tempo.io/doc/timesheets/api/rest/latest)')
-            ->addDefaultsIfNotSet()
-            ->children()
+            $node
+                ->info('Tempo timesheets (https://tempo.io/doc/timesheets/api/rest/latest)')
+                ->addDefaultsIfNotSet()
+                ->children()
                 ->arrayNode('instances')
-                    ->prototype('array')
-                    ->children()
-                        ->scalarNode('instance')->defaultNull()->end()
-                        ->scalarNode('apiToken')
-                            ->defaultNull()
-                            ->validate()
-                                ->ifEmpty()->thenInvalid('apiToken must be provided for Tempo')->end()
-                            ->end()
-                        ->end()
-                    ->end()
+                ->prototype('array')
+                ->children()
+                ->scalarNode('instance')->defaultNull()->end()
+                ->scalarNode('apiToken')
+                ->defaultNull()
+                ->validate()
+                ->ifEmpty()->thenInvalid('apiToken must be provided for Tempo')->end()
                 ->end()
-            ->end()
-        ;
+                ->end()
+                ->end()
+                ->end()
+                ->end();
 
-        return $node;
+            return $node;
+        }
     }
 }
